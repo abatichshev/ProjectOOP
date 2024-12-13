e56092227f7d893c4d3cc7bf4747c0350dc2f9d4
abatichshev <156772425+abatichshev@users.noreply.github.com>
2024-12-13 23:26:52 +0500

    Update Transcript.java
    edit Transcript.java

--- src/Transcript.java
+++ src/Transcript.java
@@ -5,7 +5,7 @@ class Transcript {
     private Map<Course, Mark> transcript;
 
     public Transcript() {
-        this.transcript = new HashMap<>(); 
+        this.transcript = new HashMap<>();
     }
 
     public Map<Course, Mark> getTranscript() {
@@ -14,5 +14,16 @@ class Transcript {
 
     public void addMark(Course course, Mark mark) {
         transcript.put(course, mark);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Transcript:\n");
+        for (Map.Entry<Course, Mark> entry : transcript.entrySet()) {
+            sb.append("Course: ").append(entry.getKey().toString())
+              .append(", Mark: ").append(entry.getValue().toString()).append("\n");
+        }
+        return sb.toString();
     }
 }
